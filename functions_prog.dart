f0d6c38b36a2695caa8ccf333a1a5e00c3b8ef6a
HasanCT282 <hasanarsalan781@gmail.com>
2025-01-20 23:19:32 +0500

    Functions in dart

---
+++ functions_prog.dart
@@ -0,0 +1,86 @@
+
+/*Functions are the block of code that performs a specific task. Here are different types of functions:
+
+No Parameter And No Return Type
+Parameter And No Return Type
+No Parameter And Return Type
+Parameter And Return Type */
+
+
+//No parameter and no return type
+void printAge(){
+  int age=23;
+  print(age);
+}
+void main(){
+  printAge();
+}
+
+/*void addNum(int num1,int num2){
+  int add=num1+num2;
+  int minus=num1-num2;
+  int multiply=num1*num2;
+ 
+  print("The result of sum is $add");
+  print("The result of difference is $minus");
+  print("PRODUCT: $multiply");
+ 
+}
+void main(){
+  addNum(23,89);
+}*/
+
+
+//Parameter And No Return Type
+
+/*void calculatePercent(int marks1,int marks2,int marks3,int marks4,int marks5){
+  double percent=((marks1+marks2+marks3+marks4+marks5)/450)*100;
+  if(marks5>50){
+    print("marks5 can not be greater than 50");
+  }else{
+  print(percent.toStringAsFixed(2));
+  } 
+}
+void main(){
+  int marks1=90;
+  int marks2=98;
+  int marks3=100;
+  int marks4=89;
+  int marks5=43;
+  calculatePercent(marks1,marks2,marks3,marks4,marks5);
+}*/
+
+//Parameter And No Return Type"
+
+/*void cubeTheNumber(double1,double2,double3){
+  double cube=double1*double2*double3;
+  print("The cube of number is $cube");
+  
+}
+void main(){
+  cubeTheNumber(12.4, 8.1, 9.9);
+}*/
+
+
+//Parameter And Return Type
+
+/*double radiusCircle(double r1,double r2 ){
+  return 3.142*r1*r2;
+}
+void main(){
+  print("--AREA---");
+  double area=radiusCircle(8.7, 9.2);
+  print(area.toStringAsFixed(2));
+}*/
+
+
+//NO paramter but return type
+
+/*String printMyName(){
+  return "Ali";
+
+}
+void main(){
+  String name=printMyName();
+  print("My name is $name");
+}*/
