76560fbd01888eab15aaaf8d538df23d2900c09d
HasanCT282 <hasanarsalan781@gmail.com>
2025-02-17 23:22:53 +0500

    Lab4(half)

--- LABS/LABS_TASK4/ques3.dart
+++ LABS/LABS_TASK4/ques3.dart
@@ -2,4 +2,22 @@
 // Write a Dart function to invert a dictionary (map), where the keys become the values and the values 
 // become the keys.
 
+void main() {
+ 
+  Map<String, String> originalMap = {
+    'a': 'apple',
+    'b': 'banana',
+    'c': 'cherry'
+  };
+
+  // Converting the dictionary: keys become values and vice versa
+  Map<String, String> reversedMap = {};
+
+  originalMap.forEach((key, value) {
+    reversedMap[value] = key;
+  });
+
+  print('Original Map: $originalMap');
+  print('Reversed Map: $reversedMap');
+}
 
